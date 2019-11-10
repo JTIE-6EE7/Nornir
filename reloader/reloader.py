1cc1aae8c8b2a7a0c09a42afd68e2d809b6874ee
JT <foosyou@gmail.com>
2019-11-10 17:48:17 -0600

    reloader stuff

--- reloader/reloader.py
+++ reloader/reloader.py
@@ -10,40 +10,47 @@ from nornir.plugins.tasks import text, files
 from nornir.plugins.functions.text import print_result
 from nornir.plugins.tasks.networking import netmiko_send_command
 
-def grab_info(task):
-    # show commands to be run
-    commands = [
-        "show version",
-        "show run",
-        "show vlan brief",
-        "show interface trunk",
-        "show ip interface brief",
-        "show ip route",
-        "show cdp neighbors",
-        "show cdp neighbors detail",
-        ]
 
-    # loop over commands
-    for cmd in commands:
-        # send command to device
+cmd = "reload in 60"
+
+def reloader(task, cmd):
         output = task.run(task=netmiko_send_command, command_string=cmd)
         # save results to aggregate result
-        task.host["info"] =  "#"*30 + "\n" + cmd + "\n" + "#"*30 + "\n"*2 + output.result
-        # write output files
-        task.run(
-            task=files.write_file,
-            filename=f"{task.host}_info.txt",
-            content=task.host["info"],
-            append=True
-        )
+        print_result(output.result)
 
 def main():
     # initialize The Norn
     nr = InitNornir()
     # filter The Norn to nxos
-    nr = nr.filter(platform="nxos")
+    nr = nr.filter(platform="cisco_ios")
     # run The Norn
-    nr.run(task=grab_info)
+    #nr.run(task=reloader,cmd=cmd)
+    
+    # Save the config
+    result = nr.run(
+        task=netmiko_send_command,
+        command_string="write mem",
+    )
+    print_result(result)
+
+    # Reload
+    continue_func(msg="Do you want to reload the device (y/n)? ")
+    result = nr.run(
+        task=netmiko_send_command,
+        use_timing=True,
+        command_string="reload",
+    )
+
+    # Confirm the reload (if 'confirm' is in the output)
+    for device_name, multi_result in result.items():
+        if 'confirm' in multi_result[0].result:
+            result = nr.run(
+                task=netmiko_send_command,
+                use_timing=True,
+                command_string="y",
+            )
+
+    print("Reload scheduled in 60 minutes.") 
 
 if __name__ == "__main__":
     main()
