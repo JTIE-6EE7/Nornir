8a982250d3f0351d9b0383a26b436afe6687edf4
JT <foosyou@gmail.com>
2019-11-10 18:01:09 -0600

    moar reload

--- reloader/reloader.py
+++ reloader/reloader.py
@@ -6,41 +6,26 @@ This script is used to collect discovery information from devices.
 
 from nornir import InitNornir
 from nornir.core.filter import F
-from nornir.plugins.tasks import text, files
 from nornir.plugins.functions.text import print_result
 from nornir.plugins.tasks.networking import netmiko_send_command
-
-
-cmd = "reload in 60"
-
-def reloader(task, cmd):
-        output = task.run(task=netmiko_send_command, command_string=cmd)
-        # save results to aggregate result
-        print_result(output.result)
 
 def main():
     # initialize The Norn
     nr = InitNornir()
     # filter The Norn to nxos
     nr = nr.filter(platform="cisco_ios")
-    # run The Norn
-    #nr.run(task=reloader,cmd=cmd)
-    
     # Save the config
     result = nr.run(
         task=netmiko_send_command,
         command_string="write mem",
-    )
-    print_result(result)
+    )    
 
-    # Reload
-    #continue_func(msg="Do you want to reload the device (y/n)? ")
+    # Reload devices in 60 minutes
     result = nr.run(
         task=netmiko_send_command,
         use_timing=True,
         command_string="reload in 60",
     )
-    print_result(result)
 
     # Confirm the reload (if 'confirm' is in the output)
     for device_name, multi_result in result.items():
@@ -48,12 +33,10 @@ def main():
             result = nr.run(
                 task=netmiko_send_command,
                 use_timing=True,
-                command_string="y",
+                command_string="",
             )
             
-    print_result(result)
-
-    print("Reload scheduled in 60 minutes.") 
+    print("Reload scheduled in 60 minutes for all devices.") 
 
 if __name__ == "__main__":
     main()
