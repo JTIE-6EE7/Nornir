04a38e69d97a65a64c926ac263a0bfc05ff025de
JT <JT@ThreeFDDI.com>
2019-11-12 08:39:44 -0600

    cancel reload

---
+++ reloader/cancel_reload.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python3
+
+'''
+This script is used to save the config and cancel a reload for all devices.
+'''
+
+from nornir import InitNornir
+from nornir.core.filter import F
+from nornir.plugins.functions.text import print_result
+from nornir.plugins.tasks.networking import netmiko_send_command
+
+def main():
+    # initialize The Norn
+    nr = InitNornir()
+    # filter The Norn to nxos
+    nr = nr.filter(platform="cisco_ios")
+    # Save the config
+    result = nr.run(
+        task=netmiko_send_command,
+        command_string="write mem",
+    )    
+
+    # Reload devices in 60 minutes
+    result = nr.run(
+        task=netmiko_send_command,
+        use_timing=True,
+        command_string="reload cancel",
+    )
+
+    print("\nConfig saved and reload cancelled for the following devices:") 
+
+    # Print hostnames
+    for device_name in result.keys():
+        print(device_name)
+            
+
+if __name__ == "__main__":
+    main()
