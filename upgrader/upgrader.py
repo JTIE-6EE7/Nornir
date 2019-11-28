4f8d31c8a06cd91f35836607547762fb5e02b9a6
JT <foosyou@gmail.com>
2019-11-27 18:52:41 -0600

    getting closer

--- upgrader/upgrader.py
+++ upgrader/upgrader.py
@@ -11,86 +11,133 @@ from nornir.plugins.tasks.networking import netmiko_send_config
 from nornir.plugins.tasks.networking import netmiko_send_command
 from nornir.plugins.tasks.networking import netmiko_file_transfer
 
-
+# Check "show version" for current software
 def check_ver(task):
 
+    # upgraded image to be used
     img_file = task.host['upgrade_img']
 
+    # run "show version" on each host
     version = task.run(
         task=netmiko_send_command,
         command_string="show version",
         use_textfsm=True,
     )
 
+    # record current software version
     current = version.result[0]['running_image']
 
+    # compare current with desired version
     if current == f"/{img_file}":
-        print(f"\nCurrent version is current:\n{current}")
+        print(f"\n{task.host} is running {current} and doe not need to be upgraded")
+        # set host upgrade flag to False
+        task.host['upgrade'] = False
     else:
-        print(f"\nUpgrade now or die.\n{current}")
+        print(f"\n{task.host} is running {current} and must be upgraded.")
+        # set host upgrade flag to True
+        task.host['upgrade'] = True
 
-
-
+# Copy IOS file to device
 def file_copy(task):
 
-    img_file = task.host['upgrade_img']
-
-#    transfer = task.run(
-#        task=netmiko_file_transfer,
-#        source_file=f"images/{img_file}",
-#        dest_file=img_file,
-#        direction='put',
-#    )
-#
-#    print_result(transfer)
-
-    switch_stack = task.run(
-        task=netmiko_send_command,
-        command_string="show switch detail",
-        use_textfsm=True,
-    )
-
-    for sw in switch_stack.result:
-        if sw['switch'] > "1":
-            print("Flash copy")
-            copy = task.run(
-                task=netmiko_send_command,
-                use_timing=True,
-                command_string=f"copy flash:/{img_file} flash{sw['switch']}:/{img_file}",
-            )
-                       
-            print(copy.result)
-
-            if 'confirm' in copy.result or 'Destination' in copy.result:
-                copy = task.run(
-                    task=netmiko_send_command,
-                    use_timing=True,
-                    command_string="",
-                )
-
-            print(copy.result)
-
-            if 'confirm' in copy.result or 'Destination' in copy.result:
-                copy = task.run(
-                    task=netmiko_send_command,
-                    use_timing=True,
-                    command_string="",
-                )
-
-            print(copy.result)
-
+    # Check if upgraded needed
+    if task.host['upgrade'] == True:
             
+        # upgraded image to be used
+        img_file = task.host['upgrade_img']
+
+        # transfer image file to switch
+        transfer = task.run(
+            task=netmiko_file_transfer,
+            source_file=f"images/{img_file}",
+            dest_file=img_file,
+            direction='put',
+        )
+
+        # print message if transfer successful
+        if transfer.result == True:
+            print(f"{task.host} IOS image file has been transferred.")
+
+        # print message if transfer fails
+        elif transfer.result == False:
+            print(f"{task.host} IOS image file transfer has failed.")
+
+        # check switch stack information
+        switch_stack = task.run(
+            task=netmiko_send_command,
+            command_string="show switch detail",
+            use_textfsm=True,
+        )
+
+        # if more than 1 switch, copy software to other switches
+        for sw in switch_stack.result:
+            if sw['switch'] > "1":
+                # file copy
+                copy = task.run(
+                    task=netmiko_send_command,
+                    use_timing=True,
+                    command_string=f"copy flash:/{img_file} flash{sw['switch']}:/test{img_file}",
+                )
+                # confirm switch responses
+                while 'confirm' in copy.result or 'Destination' in copy.result:
+                    copy = task.run(
+                        task=netmiko_send_command,
+                        use_timing=True,
+                        command_string="",
+                    )
+
+                # print message when complete
+                print(f"{task.host} flash copy to switch {sw['switch']} in progress.")
+      
+# Set switch bootvar
 def set_boot(task):
 
-    img_file = task.host['upgrade_img']
+    # Check if upgraded needed
+    if task.host['upgrade'] == True:
 
-    upgrade = task.run(
-        task=netmiko_send_config,
-        config_commands=[f"boot system flash:/{img_file}", "end", "wr mem"]
-    )       
+        # upgraded image to be used
+        img_file = task.host['upgrade_img']
 
-    print_result(upgrade)
+        # set switch bootvar to new image
+        upgrade = task.run(
+            task=netmiko_send_config,
+            config_commands=[f"boot system flash:/{img_file}", "end", "wr mem"]
+        )       
 
+        # print message if successful
+        if upgrade.failed == False:
+            print(f"{task.host} bootvar has been set.")
+
+# Reload switches
+def reload_sw(task):
+
+    confirm = "YES"
+    #confirm = input("All switches are ready for reload.\nProceed with reloading all selected switches?\nType 'YES' to continue:\n")
+    if confirm == "YES":
+        print("\n*** RELOADING ALL SELECTED SWITCHES ***\n")
+
+    # Check if upgrade reload needed
+    if task.host['upgrade'] == True:
+        print(f"{task.host} reloading...")
+
+        reload = task.run(
+            task=netmiko_send_command,
+            command_string="reload",
+            use_timing=True,
+        )        
+        
+        # Confirm the reload (if 'confirm' is in the output)
+        for host in reload.result:
+
+            if 'confirm' in reload.result:
+                task.run(
+                    task=netmiko_send_command,
+                    use_timing=True,
+                    command_string="",
+                )
+
+        print_result(reload)
+   
 def main():
 
     # initialize The Norn
@@ -107,12 +154,9 @@ def main():
 
     # run The Norn set boot
     nr.run(task=set_boot)
-    
-    reload = input("Switches are ready for reload.\nProceed with reloading all switches? (Y/N)")
-    if reload.upper() == "Y":
-        print("RELOAD!")
 
-
+    # run The Norn reload
+    nr.run(task=reload_sw)
 
 
 if __name__ == "__main__":
