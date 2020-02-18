4604621091550fdd02f2dc65731fbdf4ca0cd5b1
JT <foosyou@gmail.com>
2020-02-17 21:46:43 -0600

    bgp asn

--- bgp_route_mapper/bgp_route_mapper.py
+++ bgp_route_mapper/bgp_route_mapper.py
@@ -25,7 +25,7 @@ def get_bgp_config(task):
     # TTP template for BGP config output
     bgp_ttp_template = textwrap.dedent("""
         <group name="router_bgp">
-        router bgp {{ bgp_asn }}
+        router bgp {{ asn }}
         </group>
         <group name="networks">
          network {{ network }} mask {{ mask }}
@@ -148,7 +148,7 @@ def build_route_map(task):
     # init new config string
     new_config = ""
 
-    bgp_asn = task.host['bgp_config']['router_bgp'][0]['bgp_asn']
+    bgp_asn = task.host['bgp_config']['router_bgp'][0]['asn']
 
     # iterate over neighbors to locate route-maps for validated peers
     for neighbor in task.host['bgp_config']['neighbors']:
@@ -187,7 +187,7 @@ def build_route_map(task):
                      match as-path 1
                      set community { task.host['community'] }
                     route-map COMMUNITY_OUT deny 20                    
-                    router bgp { bgp_asn }
+                    router bgp { asn }
                      neighbor { peer_ip } route-map COMMUNITY_OUT out
                     """)
 
